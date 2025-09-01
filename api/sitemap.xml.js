4bac3283f47de30d993d61a20fe34aef00f125f0
VladDrobitko <boss.drobitko@gmail.com>
2025-09-01 12:52:25 +0200

    Add API route for sitemap.xml

---
+++ api/sitemap.xml.js
@@ -0,0 +1,20 @@
+export default function handler(req, res) {
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://helyx-media.vercel.app/</loc>
+    <lastmod>2024-01-15</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://helyx-media.vercel.app/portfolio</loc>
+    <lastmod>2024-01-15</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>
+</urlset>`;
+
+  res.setHeader('Content-Type', 'application/xml');
+  res.status(200).send(sitemap);
+}
